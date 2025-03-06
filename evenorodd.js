d2e2d0d6825bd64f14cf5070b165493246587a4f
SNEHAS20JULY <snehas20july@gmail.com>
2025-03-06 10:46:27 +0530

    Fixed Git lock issue

---
+++ evenorodd.js
@@ -0,0 +1,11 @@
+function checkEvenOdd(num) {
+    if (num % 2 === 0) {
+        console.log(num + " is Even");
+    } else {
+        console.log(num + " is Odd");
+    }
+}
+
+// Test the function
+checkEvenOdd(12); // Output: 10 is Even
+checkEvenOdd(7);  // Output: 7 is Odd
